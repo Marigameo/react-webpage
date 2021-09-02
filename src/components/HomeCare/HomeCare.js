351d3ea1f8aafebd1dea4610606ad699e950d010
Mariam Parvez <mariamparvez.m@gmail.com>
2021-09-02 20:44:58 +0530

    text changes

--- src/components/HomeCare/HomeCare.js
+++ src/components/HomeCare/HomeCare.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react'
 import './HomeCare.css'
 // import {Link} from "react-router-dom";
 import homeCareImg from '../assets/img/login-banner.png'
+import hOne from '../assets/img/home-care/01.jpg'
+import hTwo from '../assets/img/home-care/02.jpg'
+import hThree from '../assets/img/home-care/03.jpg'
+import hFour from '../assets/img/home-care/04.jpg'
+import hFive from '../assets/img/home-care/05.jpg'
+import hSix from '../assets/img/home-care/06.jpg'
 
 export default class Blog extends Component {
     render() {
@@ -11,8 +17,7 @@ export default class Blog extends Component {
             <section className="home-care-banner">
                <article className="banner-design">
                    <div className="home-care-header">
-                       <h1>Find a Doctor,<br/> Book a Medical Appointment.</h1>
-                       <p>It’s Simple and Free!</p>
+                       <h1>Professional <br/>Healthcare services,<br/>At the convenience <br/>of your Home.</h1>
                    </div>
                </article>
            </section>
@@ -39,30 +44,30 @@ export default class Blog extends Component {
             </div>
                 <article className="row text-center">
                     <div className="col-lg-4">
-                        <img src={homeCareImg} alt="home-care-img-section" />
+                        <img src={hOne} alt="home-care-img-section" />
                         <h5>Doctor’s visit</h5>
                     </div>
                     <div className="col-lg-4">
-                        <img src={homeCareImg} alt="home-care-img-section" />
+                        <img src={hTwo} alt="home-care-img-section" />
                         <h5>Health screening </h5>
                     </div>
                     <div className="col-lg-4">
-                        <img src={homeCareImg} alt="home-care-img-section" />
+                        <img src={hThree} alt="home-care-img-section" />
                         <h5>Vaccination at home</h5>
                     </div>
                 </article>
 
                 <article className="row text-center">
                     <div className="col-lg-4">
-                        <img src={homeCareImg} alt="home-care-img-section" />
+                        <img src={hFour} alt="home-care-img-section" />
                         <h5>Physical therapy</h5>
                     </div>
                     <div className="col-lg-4">
-                        <img src={homeCareImg} alt="home-care-img-section" />
+                        <img src={hFive} alt="home-care-img-section" />
                         <h5>Health monitoring after a surgery</h5>
                     </div>
                     <div className="col-lg-4">
-                        <img src={homeCareImg} alt="home-care-img-section" />
+                        <img src={hSix} alt="home-care-img-section" />
                         <h5>Health monitoring while <br/>recovering an illness</h5>
                     </div>
                 </article>
