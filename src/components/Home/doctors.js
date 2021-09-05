12ee0a6dbc6a014216de5112da4a38b8cbfd2071
Mariam Parvez <mariamparvez.m@gmail.com>
2021-09-05 13:38:29 +0530

    icons

--- src/components/Home/doctors.js
+++ src/components/Home/doctors.js
@@ -26,13 +26,39 @@ export default class MultipleItems extends Component {
       infinite: true,
       speed: 500,
       slidesToShow: 3,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
     };
     return (
       <section id="doctors">
             <div className="section-header text-center">
                 <h2>Top-Rated Doctors</h2>
-                <p>Top Healthcare Professionals to Solve Your Health Issues.</p>
+                <p className="section-p">Top Healthcare Professionals to Solve Your Health Issues.</p>
             </div>
         <Slider {...settings} className="container">
             
