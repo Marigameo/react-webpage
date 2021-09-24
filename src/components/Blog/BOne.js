aa69df26855fddafb0c004c6edccc11a9fef02d9
Mariam <mariam@medatu.com>
2021-09-24 21:10:29 +0530

    changes

--- src/components/Blog/BOne.js
+++ src/components/Blog/BOne.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React,{useEffect} from "react";
 import docOne from "../assets/img/blog/blog-1.jpg"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookmark } from '@fortawesome/free-solid-svg-icons';
 import {Link} from "react-router-dom";
 
 const BOne = () => {
+    useEffect(() => {
+        window.scroll(0,0)
+    }, )
     return ( 
+        
         <div className="col-lg-6 col-md-6 col-sm-6">
 <div className="doctor-blog profile-widget">
     <div className="doc-img">
