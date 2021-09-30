8876b9de101a451f4a031906e1a54f5d6269130b
Mariam <mariam@medatu.com>
2021-10-01 00:09:28 +0530

    changes

--- src/components/WebSite/Pages/Appointments/Appointment/Appointment.js
+++ src/components/WebSite/Pages/Appointments/Appointment/Appointment.js
@@ -1,34 +1,30 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
 import aHome from '../../../../assets/img/app/home.jpg';
 import '../Appointment.css'
 
 const Appointment = () => {
     return ( 
         <>
-        <Container className='a-banner'>
-            <Row>
-                <div className="col-lg-6 col-m-6 col-sm-6">
-                    <img src={aHome} alt="a-home" />
-                </div>
-                <div className="col-lg-6 col-m-6 col-sm-6">
-                <section className='a-content'>
-                    <h2>Download Medatu App</h2>
-                    <article className='con-content'>
-                    <p>Book an Appointment</p>
-                   
-                    <div className="input-group mb-3">
-                    <input type="text" className="form-control" placeholder="Enter your number to join the list" />
-                    <div className="input-group-append">
-                        <button className="btn btn-outline-secondary" type="button">Join</button>
-                    </div>
-                    </div>
+    <article className='a-banner'>
+    
+    <section className="a-img">
+        <img src={aHome} alt="a-home" />
+    </section>
+    
+    <section className='a-content'>
+        <h2>Download Medatu App</h2>
+        <aside className='con-content'>
+        <p>Book an Appointment</p>
+        
+        <div>
+        <input className='num-list' type="text" placeholder="Enter your number to join the list" />
+        <button type="button" class="btn btn-success">Join</button>
+        </div>
 
-                </article>
-                </section>
-                </div>
-            </Row>
-        </Container>
+        </aside>
+    </section>
+    
+    </article>
         </>
      );
 }
