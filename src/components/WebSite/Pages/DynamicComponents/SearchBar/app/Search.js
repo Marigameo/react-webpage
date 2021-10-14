30cd9e346f9fb52ecb0e340bad18aac0855e4535
Marigameo <mariappangameo@gmail.com>
2021-10-14 20:44:59 +0530

    FEAT: places autocomplete with autolocation pickup

--- src/components/WebSite/Pages/DynamicComponents/SearchBar/app/Search.js
+++ src/components/WebSite/Pages/DynamicComponents/SearchBar/app/Search.js
@@ -4,11 +4,18 @@ import PlacesAutocomplete, {
   geocodeByAddress,
   getLatLng,
 } from 'react-places-autocomplete';
- 
+import axios from "axios";
 class Search extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { address: ''};
+    this.state = { 
+      address: '',
+      currentLocation: null,
+      initialSuggestions: [],
+      latitude: null,
+      longitude: null,
+      isChanged: false,
+    };
   }
   
   componentDidMount() {
@@ -20,31 +27,69 @@ class Search extends React.Component {
       navigator.geolocation.getCurrentPosition(({ coords }) => {
         const { latitude, longitude } = coords
         console.log('latlong', latitude, longitude)
-        this.getApproxAddress(latitude, longitude)
+        this.setState({latitude: latitude, longitude: longitude})
+        this.currentLocation = this.getApproxAddress(latitude, longitude)
       })
     } else {
       console.log("Geolocation is not supported by this browser.");
     }
   }
 
+  setCurrentLocation = (data) => {
+    this.currentLocation = data
+    this.setState({address: data.address_components[0].long_name})
+  }
+
   getApproxAddress = async (lat, long) => {
     return await fetch(
-      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&result_type=street_address|political|locality|sublocality|neighborhood|premise|subpremise&key=AIzaSyCvxiUM4shn9Zd3WzBQUVbxpQhLeMgA6BE&libraries=places`
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&result_type=street_address|political|locality|sublocality|neighborhood|premise|subpremise&key=AIzaSyBgZl9hJTsIqoYnfZmmFDi6oKqg3QNYN5s&libraries=places`
     )
       .then((response) => response.json())
       .then(async (data) => {
         console.log('Map Response: ', data)
         if (data.status === 'OK' && data.results.length > 0) {
-          return data.results[0]
+          this.setCurrentLocation(data.results[0])
         }
    
         return null
         
       })
   }
+  getNearbyPlaces = async () => {
+    this.setState({isChanged: false})
+    const {latitude, longitude} = this.state
+    console.log(latitude, longitude)
+    const URL = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${
+      latitude
+    },${longitude}&type=bus_station|post_office|tourist_attraction|train_station|hospital&radius=${5000 *
+      1000}&key=AIzaSyBgZl9hJTsIqoYnfZmmFDi6oKqg3QNYN5s`;
+    axios
+      .get(URL)
+      .then(response => {
+        console.log(response.data.results);
+        this.formatSuggestions(response.data.results)
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+  }
+
+  formatSuggestions = (results) => {
+    const formattedResults = results 
+    .map(function(k) {
+      return {
+        description: k.name,
+        id: k.place_id,
+        types: ["locality", "political", "geocode"],
+        formattedSuggestion: {mainText: k.name, secondaryText: k.vicinity}
+      }
+    })
+    console.log('formattedResults', formattedResults)
+    this.setState({initialSuggestions: formattedResults})
+  }
 
   handleChange = address => {
-    console.log('address', address)
+    this.setState({isChanged: true})
     this.setState({ address });
   };
  
@@ -54,7 +99,12 @@ class Search extends React.Component {
       .then(latLng => console.log('Success', latLng))
       .catch(error => console.error('Error', error));
   };
- 
+
+  fetchSuggestions = () => {
+    console.log('fetching suggestions')
+    this.getNearbyPlaces()
+  }
+
   render() {
     return (
       <PlacesAutocomplete
@@ -62,17 +112,20 @@ class Search extends React.Component {
         onChange={this.handleChange}
         onSelect={this.handleSelect}
       >
-        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-          <div>
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => {
+          console.log('suggesstions', suggestions)
+          const iterableSuggestion = this.state.isChanged ? suggestions : this.state.initialSuggestions
+          return(
+            <div>
             <div className="search-box" style={{padding: "0"}}>
            
             <form className="form-design" action="assets/webpages/list.html">
 
             <div className='search-location'>
-            <input  {...getInputProps({placeholder: 'Search Places ...', className: 'location-search-input',})}type="text" id="location" placeholder="Search Location"/>                
+            <input  {...getInputProps({placeholder: 'Search Places ...', className: 'location-search-input',})}type="text" id="location" placeholder="Search Location" onFocus={this.fetchSuggestions}/>                
               <div className="autocomplete-dropdown-container">
                   {loading && <div>Loading...</div>}
-                  {suggestions.map(suggestion => {
+                  {iterableSuggestion.map(suggestion => {
                       const className = suggestion.active
                       ? 'suggestion-item--active'
                       : 'suggestion-item';
@@ -101,7 +154,10 @@ class Search extends React.Component {
             </form>
           </div>
         </div>
-        )}
+          )
+        }
+         
+        }
       </PlacesAutocomplete>
     );
    
